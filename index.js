630c565f7ff5457962b7787ebb60673996e6b41f
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2022-01-24 15:00:15 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.send();
+});
+
+app.listen(process.env.PORT,() => {
+  console.log(`Escutando na porta ${process.env.PORT}`);
+});
