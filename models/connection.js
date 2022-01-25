6c6f706a4b18d0916cc89be6e1a2fdbfec7833ab
Elithon Silva <elithonsilva@gmail.com>
2022-01-25 15:44:35 -0300

    cria arquivo connection.js

---
+++ models/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+});
+
+module.exports = connection;
