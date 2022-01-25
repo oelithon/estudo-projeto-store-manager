33df102f44442b12f0b2fc890c665987ed20b91e
Elithon Silva <elithonsilva@gmail.com>
2022-01-25 19:23:56 -0300

    implementa o router.get para chamar a função getProducts

--- routers/routers.js
+++ routers/routers.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const controllerProducts = require('../controllers/controllerProtucts');
+
 router
   .route('/products')
-  .post(() => { });
+  .post(controllerProducts.createProduct)
+  .get(async (req, res) => {
+    const test = await controllerProducts.getProducts();
+    console.log(test);
+    res.status(200).json(test);
+  });
 
 module.exports = router;
