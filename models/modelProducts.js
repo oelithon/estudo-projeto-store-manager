ad5a928fb9025186e747963c37cb7a43de4c3fcb
Elithon Silva <elithonsilva@gmail.com>
2022-01-25 19:17:37 -0300

    implementa função getProducts e faz exportação

--- models/modelProducts.js
+++ models/modelProducts.js
@@ -3,13 +3,24 @@ const connection = require('./connection');
 const createProduct = async (name, quantity) => {
   await connection.execute(
     `
-  INSERT INTO products (name, quantity)
+  INSERT INTO StoreManager.products (name, quantity)
   VALUE
     (?, ?)
   `, [name, quantity],
   );
 };
 
+const getProducts = async () => {
+  try {
+    const products = await connection.execute('SELECT * FROM StoreManager.products');
+    console.log(products);
+    return products;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 module.exports = {
   createProduct,
+  getProducts,
 };
