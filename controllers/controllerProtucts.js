cd08a8a71993a720248f4329ca9ed901c0162d58
Elithon Silva <elithonsilva@gmail.com>
2022-01-26 11:53:22 -0300

    remove as validações do controller para um middleware, chama o middleware de validação no controller

--- controllers/controllerProtucts.js
+++ controllers/controllerProtucts.js
@@ -1,23 +1,12 @@
 const serviceProduct = require('../services/serviceProduct');
+const validation = require('../middlewares/validation');
 
-const createProduct = async (req, res) => {
+const createProduct = async (req, res, next) => {
   const { name, quantity } = req.body;
-  if (!name) {
-    res.status(400).json({ message: '"name" is required' });
-    return;
-  }
-  if (!quantity && quantity !== 0) {
-    res.status(400).json({ message: '"quantity" is required' });
-    return;
-  }
-  if (name.length < 5) {
-    res.status(422).json({ message: '"name" length must be at least 5 characters long' });
-    return;
-  }
-  if (typeof quantity !== Number || quantity < 1) {
-    res.status(422).json({ message: '"quantity" must be a number larger than or equal to 1' });
-    return;
-  }
+
+  validation.valueRequired(req, res, next);
+  validation.inputRequirements(req, res, next);
+
   await serviceProduct.createProduct(name, quantity);
   res.status(201).json({ id: 1, name: `${name}`, quantity: `${quantity}` });
 };
