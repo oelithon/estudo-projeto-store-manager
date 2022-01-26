bbc63f1c82c943311812aa4aca102ddf262d0d68
Elithon Silva <elithonsilva@gmail.com>
2022-01-26 15:36:05 -0300

    retiva as funçoes de validation do controller

--- controllers/controllerProtucts.js
+++ controllers/controllerProtucts.js
@@ -1,14 +1,10 @@
 const serviceProduct = require('../services/serviceProduct');
-const validation = require('../middlewares/validation');
 
-const createProduct = async (req, res, next) => {
+const createProduct = async (req, res) => {
   const { name, quantity } = req.body;
 
-  validation.valueRequired(req, res, next);
-  validation.inputRequirements(req, res, next);
-
   await serviceProduct.createProduct(name, quantity);
-  res.status(201).json({ id: 1, name: `${name}`, quantity: `${quantity}` });
+  res.status(201).json({ message: 'produto cadastrado' });
 };
 
 const getProducts = async () => {
