0cca1e2cc1d239b96024c709de3ec84723169b5d
Elithon Silva <elithonsilva@gmail.com>
2022-01-26 15:36:34 -0300

    faz require das funçoes validation

--- routers/routers.js
+++ routers/routers.js
@@ -2,15 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+const validation = require('../middlewares/validation');
 const controllerProducts = require('../controllers/controllerProtucts');
 
 router
   .route('/products')
-  .post(controllerProducts.createProduct)
+  .post(
+    validation.valueRequired,
+    validation.inputRequirements,
+    controllerProducts.createProduct,
+  )
   .get(async (req, res) => {
-    const test = await controllerProducts.getProducts();
-    console.log(test);
-    res.status(200).json(test);
+    const products = await controllerProducts.getProducts();
+    res.status(200).json(products);
   });
 
 module.exports = router;
