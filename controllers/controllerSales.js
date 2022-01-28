e14af11c42dcd9172fcd5d4f9bd3ff41c06ba6d3
Elithon Silva <elithonsilva@gmail.com>
2022-01-27 23:16:09 -0300

    cria e implementa a funcao createSalesProducts

--- controllers/controllerSales.js
+++ controllers/controllerSales.js
@@ -1,1 +1,25 @@
-const controllerSales = require('../services/serviceSales');
+const serviceSales = require('../services/serviceSales');
+
+const createSalesProducts = async (req, res) => {
+  const salesProduct = req.body;
+  const id = await serviceSales.createSales();
+
+  const salesPromise = salesProduct.map(async (sale) => {
+    const { product_id, quantity } = sale;
+    const sales = await serviceSales.createSalesProducts(id, product_id, quantity);
+    return sales;
+  });
+
+  await Promise.all(salesPromise);
+
+  res.status(201).json(
+    {
+      id,
+      itemsSold: salesProduct,
+    },
+  );
+};
+
+module.exports = {
+  createSalesProducts,
+};
