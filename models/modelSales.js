5b63f0ff1e8340eb3bb63beb56b2615cb11adc30
Elithon Silva <elithonsilva@gmail.com>
2022-01-27 23:11:37 -0300

    cria e implemanta as funções createSales e createSalesProducts com suas respectivas query

--- models/modelSales.js
+++ models/modelSales.js
@@ -1,13 +1,28 @@
 const connection = require('./connection');
 
-const createSaleProducts = async (product) => {
-  await connection.execute(
+const createSales = async () => {
+  const [salesId] = await connection.execute(
     `
-    INSERT INTO StoreManager.products (name, quantity)
-    `
+    INSERT INTO StoreManager.sales ()
+    VALUE
+      ()
+    `,
   );
+  return salesId.insertId;
+};
+
+const createSalesProducts = async (id, productId, quantity) => {
+  const [sales] = await connection.execute(
+    `
+    INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
+    VALUES
+      (?, ?, ?)
+    `, [id, productId, quantity],
+  );
+  return sales;
 };
 
 module.exports = {
-  createSaleProducts,
+  createSales,
+  createSalesProducts,
 };
