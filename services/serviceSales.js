83d4472d3b2351aaff734c52f3e6bf96b4f0d025
Elithon Silva <elithonsilva@gmail.com>
2022-01-27 23:12:33 -0300

    faz require das funções createSales e createSalesProducts, implementa e exporta

--- services/serviceSales.js
+++ services/serviceSales.js
@@ -1,1 +1,16 @@
 const modelSales = require('../models/modelSales');
+
+const createSales = async () => {
+  const salesId = await modelSales.createSales();
+  return salesId;
+};
+
+const createSalesProducts = async (id, productId, quantity) => {
+  const sales = await modelSales.createSalesProducts(id, productId, quantity);
+  return sales;
+};
+
+module.exports = {
+  createSales,
+  createSalesProducts,
+};
