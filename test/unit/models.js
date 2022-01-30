993f621a784eb9b877ddb24cc0d8496719cd28f9
Elithon Silva <elithonsilva@gmail.com>
2022-01-30 19:18:24 -0300

    teste verifica criaçao de products e sales

--- test/unit/models.js
+++ test/unit/models.js
@@ -2,24 +2,51 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const connection = require('../../models/connection');
 const modelProducts = require('../../models/modelProducts');
+const modelSales = require('../../models/modelSales');
 
-describe('Products Model', () => {
-  describe('Não existe produtos cadastrados no banco de dados', async () => {
-    before(async () => {
-      const mock = [[], [{}, {}]];
+describe('modelProducts', () => {
+
+  describe('endpoint para listar os produtos', () => {
+    before(() => {
+      const mock = [[{
+        id: 1,
+        name: "produto A",
+        quantity: 10
+      }], []];
       sinon.stub(connection, 'execute').resolves(mock);
     });
-    after(async () => {
+    after(() => {
+      connection.execute.restore();
+    });
+    it('a requisição é feita corretamente', async () => {
+      const products = await modelProducts.getProducts();
+      expect(products).to.be.an('array');
+    });
+  });
+});
+
+describe('modelSales', () => {
+  describe('endpoint para cadastrar vendas', () => {
+    before(() => {
+      const mock = [[
+        {
+          "product_id": 1,
+          "quantity": 2
+        },
+        {
+          "product_id": 2,
+          "quantity": 5
+        }
+      ], []];
+      sinon.stub(connection, 'execute').resolves(mock);
+    });
+    after(() => {
       connection.execute.restore();
     });
 
-    if ('retorna um array vazio', async () => {
-      const products = await modelProducts.getProducts();
-      expect(products).to.be.empty;
-    });
-    if ('retorna um array de objetos', () => {
-      const products = await modelProducts.getProducts();
-      expect(products).to.be.an('array');
+    it('a requisição é feita corretamente', async () => {
+      const createSales = await modelSales.createSalesProducts();
+      expect(createSales).to.be.an('array');
     });
   });
 });
