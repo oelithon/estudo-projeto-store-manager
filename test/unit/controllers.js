ceab21a4297c46c9a41fdbf21d05bf1e96a44c04
Elithon Silva <elithonsilva@gmail.com>
2022-01-31 10:43:21 -0300

    refatora o arquivo de testes para deixar melhor legivel

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -7,93 +7,97 @@ const serviceSales = require('../../services/serviceSales');
 const controllerSales = require('../../controllers/controllerSales');
 const { equal } = require('@hapi/joi/lib/base');
 
-describe('endpoint para listar os produtos', () => {
-  const response = {};
-  const request = {};
+describe('testes na camada controllerProducts', () => {
+  describe('endpoint para listar os produtos', () => {
+    const response = {};
+    const request = {};
 
-  before(() => {
+    before(() => {
 
-    response.status = sinon.stub().returns(response);
-    response.json = sinon.stub().returns();
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
 
-    sinon.stub(serviceProduct, 'getProducts').resolves(true);
+      sinon.stub(serviceProduct, 'getProducts').resolves(true);
+    });
+    after(() => {
+      serviceProduct.getProducts.restore();
+    });
+
+    it('será validado que todos produtos estão sendo retornados.', async () => {
+      await controllerProducts.getProducts(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    before(() => {
+      request.params = [{ id: 1 }];
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(serviceProduct, 'getProductId').resolves(true);
+    });
+    after(() => {
+      serviceProduct.getProductId.restore();
+    });
+
+    it('será validado que é possível listar um determinado produto.', async () => {
+      await controllerProducts.getProductId(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
   });
-  after(() => {
-    serviceProduct.getProducts.restore();
-  });
 
-  it('será validado que todos produtos estão sendo retornados.', async () => {
-    await controllerProducts.getProducts(request, response);
+  describe('endpoint para atualizar um produto', () => {
+    const response = {};
+    const request = {};
 
-    expect(response.status.calledWith(200)).to.be.equal(true);
-  });
+    before(() => {
+      request.params = [{ id: 1 }];
+      request.body = [{ name: 'produtoTeste', quantity: 5 }];
 
-  before(() => {
-    request.params = [{ id: 1 }];
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
 
-    response.status = sinon.stub().returns(response);
-    response.json = sinon.stub().returns();
+      sinon.stub(serviceProduct, 'updateProduct').resolves(true);
+      sinon.stub(serviceProduct, 'getProductId').resolves(true);
+    });
+    after(() => {
+      serviceProduct.updateProduct.restore();
+      serviceProduct.getProductId.restore();
+    });
 
-    sinon.stub(serviceProduct, 'getProductId').resolves(true);
-  });
-  after(() => {
-    serviceProduct.getProductId.restore();
-  });
+    it('quando a requisição é feita corretamente, o produto deve ser alterado.', async () => {
+      await controllerProducts.updateProduct(request, response);
 
-  it('será validado que é possível listar um determinado produto.', async () => {
-    await controllerProducts.getProductId(request, response);
-
-    expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
   });
 });
 
-describe('endpoint para atualizar um produto', () => {
-  const response = {};
-  const request = {};
+describe('testes na camada controllerSales', () => {
+  describe('endpoint para cadastrar vendas', () => {
+    const response = {};
+    const request = {};
 
-  before(() => {
-    request.params = [{ id: 1 }];
-    request.body = [{ name: 'produtoTeste', quantity: 5 }];
+    before(() => {
+      request.body = [{ product_id: 1, quantity: 10 }];
 
-    response.status = sinon.stub().returns(response);
-    response.json = sinon.stub().returns();
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
 
-    sinon.stub(serviceProduct, 'updateProduct').resolves(true);
-    sinon.stub(serviceProduct, 'getProductId').resolves(true);
-  });
-  after(() => {
-    serviceProduct.updateProduct.restore();
-    serviceProduct.getProductId.restore();
-  });
+      sinon.stub(serviceSales, 'createSales').resolves(true);
+      sinon.stub(serviceSales, 'createSalesProducts').resolves(true);
+    });
+    after(() => {
+      serviceSales.createSales.restore();
+      serviceSales.createSalesProducts.restore();
+    });
 
-  it('quando a requisição é feita corretamente, o produto deve ser alterado.', async () => {
-    await controllerProducts.updateProduct(request, response);
+    it('é chamado o status com o código 201', async () => {
+      await controllerSales.createSalesProducts(request, response);
 
-    expect(response.status(200)).to.be.equal(true);
-  });
-});
-
-describe('endpoint para cadastrar vendas', () => {
-  const response = {};
-  const request = {};
-
-  before(() => {
-    request.body = [{ product_id: 1, quantity: 10 }];
-
-    response.status = sinon.stub().returns(response);
-    response.json = sinon.stub().returns();
-
-    sinon.stub(serviceSales, 'createSales').resolves(true);
-    sinon.stub(serviceSales, 'createSalesProducts').resolves(true);
-  });
-  after(() => {
-    serviceSales.createSales.restore();
-    serviceSales.createSalesProducts.restore();
-  });
-
-  it('é chamado o status com o código 201', async () => {
-    await controllerSales.createSalesProducts(request, response);
-
-    expect(response.status.calledWith(201)).to.be.equal(true);
+      expect(response.status.calledWith(201)).to.be.equal(true);
+    });
   });
 });
